dff19a0964f44f74bc3291d37dfdaf927ae485ac
Bruno Belloni <bruno.belloni@hotmail.com>
2018-04-19 23:01:44 -0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+from math import sqrt
+from Methods import Cordenadas
+from Methods import CorRandomica
+
+imagem = cv2.imread('imagens/4.jpg')
+
+cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+ret, thresh = cv2.threshold(cinza, 100, 255, cv2.THRESH_BINARY_INV)
+
+'''
+# Threshold
+# Morfologia, Erosão, Dilatação...
+
+kernel = np.ones((2, 2), np.uint8)
+opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations = 2)
+'''
+
+thresh, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+Xa, Ya, Ra = Cordenadas(cinza)
+
+contador = 0
+for contour in contours:
+    area = round(cv2.contourArea(contour), 1)
+
+    if area > 100 and area < 2500:
+        (Xb, Yb), Rb = cv2.minEnclosingCircle(contour) # x, y e raio #
+        distAB = sqrt(((Xa - Xb) ** 2) + ((Ya - Yb) ** 2))
+
+        if(distAB < Ra - 500):
+            per = round(cv2.arcLength(contour, True) , 1) # Perimetro #
+            contador = contador + 1
+
+            cv2.drawContours(imagem, contour, -1, CorRandomica(), 2)
+            cv2.putText(imagem, "A: " + str(area), (int(Xa), int(Ya)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+            cv2.putText(imagem, "P: " + str(per), (int(Xa), int(Ya) + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+
+cv2.imwrite("Final " + str(contador) + ".jpg", imagem)
+
+print("Ok")
---
+++ Methods.py
@@ -0,0 +1,27 @@
+import cv2
+import random
+import numpy as np
+
+def Cordenadas(imagem):
+    ret, thresh = cv2.threshold(imagem, 130, 255, cv2.THRESH_BINARY_INV)
+    circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 22, 1000000) # Metodo que detecta os circulos
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        x, y, r = 0, 0, 0
+        for i in circles[0,:]:
+            if(r < i[2]):
+                x, y, r = i[0], i[1], i[2]
+        cv2.circle(imagem, (x, y), r, (0, 0, 255), 2)
+        return x, y, r
+    else:
+        return 0
+
+def CorRandomica():
+    sort = random.randint(1, 3)
+    if(sort == 1):
+        return (255, 0, 0)
+    elif(sort == 2):
+        return (0, 255, 0)
+    elif(sort == 3):
+        return (0, 0, 255)
