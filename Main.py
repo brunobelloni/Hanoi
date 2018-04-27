eaa837077dcc59456aaf51d80b68e4ceecced22d
Bruno Belloni <bruno.belloni@hotmail.com>
2018-04-27 13:43:28 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,37 +17,32 @@ closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
 thresh, contours, hierCirclerchy = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 xCircle, yCircle, rCircle = cordenadas(cinza)
 
-# arrayA = []
-# arrayP = []
-
 vetor = []
 
 contador = 0
 for contour in contours:
-    area = round(cv2.contourArea(contour), 1)
+    area = cv2.contourArea(contour)
 
     if 120 < area < 3500:
         (xA, yA), rA = cv2.minEnclosingCircle(contour)
         distAB = sqrt(((xCircle - xA) ** 2) + ((yCircle - yA) ** 2))
 
         if distAB < rCircle - 300:
-            per = round(cv2.arcLength(contour, True), 1)
+            per = cv2.arcLength(contour, True)
             contador = contador + 1
-            # arrayA.append(area)
-            # arrayP.append(per)
-
-            balance = (area * 0.7) + (per * 0.3)
-            vetor.append(balance)
-
-            margin = 40
+            # balance = (area * 0.7) + (per * 0.3)
+            vetor.append(area)
+            margin = 25
             y, y1 = int(yA) - int(rA + margin), int(yA) + int(rA + margin)
             x, x1 = int(xA) - int(rA + margin), int(xA) + int(rA + margin)
 
-            if(98 < balance < 832):
+            tipo = ""
+            cor = (0, 0, 0)
+            if(120 < area < 1125.5):
                 tipo, cor = "1", (255, 0, 0)
-            elif(832 <= balance < 1566):
+            elif(1125.5 <= area < 2130.5):
                 tipo, cor = "2", (0, 255, 0)
-            elif(area >= 1566):
+            elif(area >= 2130.5):
                 tipo, cor = "3", (0, 0, 255)
 
             cv2.imwrite("Classificacao\\Tipo " + tipo + "\\" + str(contador) + ".jpg", copy[y:y1, x:x1])
@@ -56,11 +51,13 @@ for contour in contours:
             # cv2.putText(imagem, "P: " + str(per), (int(xA), int(yA) + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 
 cv2.imwrite("Final " + str(contador) + ".jpg", imagem)
-print("Final " + str(contador))
+print("Identificações: " + str(contador))
 
+'''
 sns.set()
-plt.hist(vetor, bins = 300)
-plt.xlabel('Area/Perimetro')
+plt.hist(vetor, bins = 3)
+plt.xlabel('Balance')
 plt.ylabel('Quantidade')
-plt.title("HistogrCirclema Area/Perimetro")
+plt.title("Histograma")
 plt.show()
+'''
